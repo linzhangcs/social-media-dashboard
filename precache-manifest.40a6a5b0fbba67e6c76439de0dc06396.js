97d5d99a8f611614f333c511a751ad27e918cad6
Lin Zhang <lin.zhang.ux@gmail.com>
2020-07-23 15:31:32 -0700

    Updates

---
+++ precache-manifest.40a6a5b0fbba67e6c76439de0dc06396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09001c7a9e6c123986b3deda3544142",
+    "url": "/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "d092f6cc043fc1e8cb8b",
+    "url": "/social-media-dashboard/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "50a6f9517674ef8a6be7",
+    "url": "/social-media-dashboard/static/js/2.ba97eb4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard/static/js/2.ba97eb4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d092f6cc043fc1e8cb8b",
+    "url": "/social-media-dashboard/static/js/main.1f8258f2.chunk.js"
+  },
+  {
+    "revision": "0284566f6156379b65ff",
+    "url": "/social-media-dashboard/static/js/runtime-main.e6eaf8f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social-media-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
