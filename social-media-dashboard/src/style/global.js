914314364c90de239d3902a32e8c19657bae7905
Lin Zhang <lin.zhang.ux@gmail.com>
2020-07-23 22:33:57 -0700

    add style folder for global style files

---
+++ social-media-dashboard/src/style/global.js
@@ -0,0 +1,33 @@
+export const colors = {
+    primaryLimeGreen: 'hsl(163, 72%, 41%)',
+    primaryBrightRed: 'hsl(356, 69%, 56%)',
+    facebook: 'hsl(208, 92%, 53%)',
+    twitter: 'hsl(203, 89%, 53%)',
+    instagramGradientOne: 'hsl(37, 97%, 70%)',
+    instagramGradientTwo: 'hsl(329, 70%, 58%)',
+    youtube: 'hsl(348, 97%, 39%)',
+};
+
+export const darkTheme = {
+    veryDarkBlueBg: 'hsl(230, 17%, 14%)',
+    veryDarkBlueTopBg: 'hsl(232, 19%, 15%)',
+    darkBlue: 'hsl(228, 28%, 20%)',
+    blueText: 'hsl(228, 34%, 66%)',
+    white: 'hsl(0, 0%, 100%)',
+    toggleGradientOne:'hsl(210, 78%, 56%)',
+    toggleGradientTwo: 'hsl(146, 68%, 55%)'
+}
+
+export const lightTheme = {
+    whiteBg: 'hsl(0, 0%, 100%)',
+    paleBlue: 'hsl(225, 100%, 98%)',
+    lightGrayishBlueCard: 'hsl(227, 47%, 96%)',
+    darkGrayishBlueText: 'hsl(228, 12%, 44%)',
+    darkBlueText: 'hsl(230, 17%, 14%)',
+    toggle: 'hsl(230, 22%, 74%)'
+}
+
+export const breakpoints = {
+    mobile: '375px',
+    desktop: '1440px',
+}
