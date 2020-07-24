36431c410aaf50c43b88dd4b49e85323468e9dcb
Lin Zhang <lin.zhang.ux@gmail.com>
2020-07-23 22:36:13 -0700

    import components to the app

--- social-media-dashboard/src/App.js
+++ social-media-dashboard/src/App.js
@@ -1,64 +1,29 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import socialMediaData from './data/socialMediaData.json';
+import Dashboard from './components/Dashboard';
+import DashboardHeader from './components/DashboardHeader';
 
-function Dashboard({data}) {
-  console.log(data);
-  return(
-    <>
-    <div>Dashboard</div>
-    <p>{data.totalFollower}</p>
-    </>
-  );
-}
+import styled from 'styled-components';
+import { darkTheme, lightTheme } from './style/global.js';
+
+const statusData = socialMediaData;
+const Body = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-item: center;
+  color: ${darkTheme.white};
+  background-color: ${darkTheme.veryDarkBlueBg};
+  font-size: 14px;
+`;
+
 function App() {
-  const statusData = {
-    totalFollower: 23004,
-    socialMedia: {
-      facebook: {
-        handle: "@nathanf",
-        followers: 1987,
-        changes: 12,
-        enagement: 87,
-        enagementChanges: 3,
-        likes: 52,
-        likesChanges: -2
-      },
-      twitter: {
-        handle: "@nathanf",
-        followers: 1044,
-        changes: 99,
-        enagement: 117,
-        enagementChanges: 303,
-        likes: 507,
-        likesChanges: 553
-      },
-      instagram: {
-        handle: "@realnathanf",
-        followers: "11k",
-        changes: 1099,
-        enagement: "52K",
-        enagementChanges: 1375,
-        likes: 5462,
-        likesChanges: 2257
-      },
-      youtube: {
-        handle: "Nathan F.",
-        followers: 8239,
-        changes: -144,
-        enagement: 1407,
-        enagementChanges: -12,
-        likes: 107,
-        likesChanges: -19
-      }
-    }
-  }
   return (
-    <div className="App">
-      <header className="App-header">
+      <Body>
+        <DashboardHeader></DashboardHeader>
         <Dashboard data={statusData}></Dashboard>
-      </header>
-    </div>
+      </Body>
   );
 }
 
