cf5831cbbc5eb1bdb7e67c59d72780bd7c861821
Lin Zhang <lin.zhang.ux@gmail.com>
2020-07-23 21:21:13 -0700

    add json data for passing dashboard info to dashboard component

--- social-media-dashboard/src/App.js
+++ social-media-dashboard/src/App.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Dashboard({data}) {
+  console.log(data);
+  return(
+    <>
+    <div>Dashboard</div>
+    <p>{data.totalFollower}</p>
+    </>
+  );
+}
 function App() {
+  const statusData = {
+    totalFollower: 23004,
+    socialMedia: {
+      facebook: {
+        handle: "@nathanf",
+        followers: 1987,
+        changes: 12,
+        enagement: 87,
+        enagementChanges: 3,
+        likes: 52,
+        likesChanges: -2
+      },
+      twitter: {
+        handle: "@nathanf",
+        followers: 1044,
+        changes: 99,
+        enagement: 117,
+        enagementChanges: 303,
+        likes: 507,
+        likesChanges: 553
+      },
+      instagram: {
+        handle: "@realnathanf",
+        followers: "11k",
+        changes: 1099,
+        enagement: "52K",
+        enagementChanges: 1375,
+        likes: 5462,
+        likesChanges: 2257
+      },
+      youtube: {
+        handle: "Nathan F.",
+        followers: 8239,
+        changes: -144,
+        enagement: 1407,
+        enagementChanges: -12,
+        likes: 107,
+        likesChanges: -19
+      }
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Dashboard data={statusData}></Dashboard>
       </header>
     </div>
   );
